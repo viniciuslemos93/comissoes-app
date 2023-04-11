98b391904eccd52c268904ffb5f33fbcc1a1e5da
Vinicius Lemos <vinycius1993@gmail.com>
2023-04-11 15:28:36 -0300

    feat: desenvolvido a lógica para inserir cada funcionário no container

---
+++ assets/scripts/script.js
@@ -0,0 +1,25 @@
+const tarefas = window.document.getElementById('container-funcionarios')
+
+function inserirNovoFuncionario() {
+    const novoFuncionario = window.document.getElementById('novo-funcionario')
+    const addFuncionario = novoFuncionario.value
+    const funcionarios = window.document.getElementById('container-funcionarios')
+    
+
+    if (addFuncionario == "") {
+        alert("Campo vazio, por favor insira uma Tarefa.")
+
+    } else {
+        const cadaFuncionario = `
+            <div class="cadaFuncionario">
+            <p class="funcionario">
+            ${addFuncionario}
+            </p>            
+            
+            </div>
+            `
+funcionarios.insertAdjacentHTML('beforeend', cadaFuncionario);        
+        novoFuncionario.value = ''
+        novoFuncionario.focus()
+    }
+}
