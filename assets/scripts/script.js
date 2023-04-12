a644d6f41ef08d28963131832dfb5d3c844b374c
Vinicius Lemos <vinycius1993@gmail.com>
2023-04-12 11:43:43 -0300

    refactor: ajustado cadastro de cada funcionário

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,19 +3,18 @@ const tarefas = window.document.getElementById('container-funcionarios')
 function inserirNovoFuncionario() {
     const novoFuncionario = window.document.getElementById('novo-funcionario')
     const addFuncionario = novoFuncionario.value
-    const funcionarios = window.document.getElementById('container-funcionarios')
-    
+    const funcionarios = window.document.getElementById('container-funcionarios')    
 
     if (addFuncionario == "") {
         alert("Campo vazio, por favor insira um Funcionário.")
 
     } else {
         const cadaFuncionario = `
-            <div class="cadaFuncionario">
+            <div class="d-flex">
             <p class="funcionario">
             ${addFuncionario}
             </p>            
-            
+            <input type="number" class="percentual">
             </div>
             `
 funcionarios.insertAdjacentHTML('beforeend', cadaFuncionario);        
